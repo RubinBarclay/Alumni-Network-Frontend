43937441445717c9a091935591fcde3f6834b0d9
Rubin Barclay <63505731+RubinBarclay@users.noreply.github.com>
2023-04-11 16:02:02 +0200

    refactor: changed folder structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Pro tip: Conditional configs based on environment (dev, prod, etc.)
+// https://vitejs.dev/config/#conditional-config
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000
+  }
 })
